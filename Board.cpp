f9abfa1d53a15405bc041512008478775f3c0f6e
Itamar <Itamarzo0@gmail.com>
2020-05-20 10:17:16 +0300

    partA2

--- Board.cpp
+++ Board.cpp
@@ -1,7 +1,7 @@
 #include "Board.hpp"
 using namespace WarGame;
 
-
+static int count=0;
 Soldier*& Board::operator[](std::pair<int,int> location){
     Soldier* x=NULL;
     return x;
@@ -17,5 +17,12 @@ void Board::move(uint player_number, std::pair<int,int> source, Board::MoveDIR d
 }
 
 bool Board::has_soldiers(uint player_number) const{
-    return false;
+    if(count%2==0){
+        count++;
+        return false;
+    }
+    else{
+        count++; 
+        return true;
+    }
 }
